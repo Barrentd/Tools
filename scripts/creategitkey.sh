9d9a2de33a4dc6017a586abd12a288c6991258f9
Bertrand Vlt <bertrand.valat@gmail.com>
2022-11-25 17:14:22 +0100

    Update creategitkey.sh

--- scripts/creategitkey.sh
+++ scripts/creategitkey.sh
@@ -1,3 +1,3 @@
 mkdir -p ~/.ssh/github
 ssh-keygen -t ed25519 -f ~/.ssh/github/id_ed25519 -q -N ""
-printf "\nHost github.com\n   Port 22\n   User git\n   IdentityFile ~/.ssh/gitlab/id_ed25519\n" >> ~/.ssh/config
+printf "\nHost github.com\n   Port 22\n   User git\n   IdentityFile ~/.ssh/github/id_ed25519\n" >> ~/.ssh/config
